65e71fb094573ee59e01c3a3d2b5e265f2a87aab
jonas.oster <jonas.oster@d3ffdab7-2956-0410-b283-a12d15995259>
2008-09-07 12:45:21 +0000

    Added menu bar and reload capability to GUI.

--- Gui.cs
+++ Gui.cs
@@ -5,19 +5,21 @@ using System.Collections.Generic;
 using Cairo;
 using Gtk;
 
-class DiagramWin : DrawingArea
+class DiagramArea : DrawingArea
 {
-    Diagram diagram;
-    
-    public DiagramWin (string file_name)
-    {
-        diagram = Program.Load(file_name);
+    private Diagram diagram;
+    public Diagram Diagram {
+        get { return diagram; }
+        set
+        {
+            diagram = value;
+            QueueDraw();
+        }
+    }
 
-        Window win = new Window ("Smul");
-        win.SetDefaultSize (595, 400);
-        win.DeleteEvent += new DeleteEventHandler (OnQuit);
-        win.Add (this);
-        win.ShowAll ();
+    private int error_counter;
+    public int ErrorCounter {
+        get { return error_counter; }
     }
     
     void Draw (Context cr, int width, int height)
@@ -27,12 +29,19 @@ class DiagramWin : DrawingArea
         cr.Rectangle(0, 0, width, height);
         cr.Fill();
 
-        if(diagram != null)
+        try
         {
-            DiagramRenderer dr = new DiagramRenderer(cr, width, height, diagram);
-            cr.Color = new Color(0.0, 0.0, 0.0);
-            dr.Render();
-            dr.Draw();
+            if(diagram != null)
+            {
+                DiagramRenderer dr = new DiagramRenderer(cr, width, height, diagram);
+                cr.Color = new Color(0.0, 0.0, 0.0);
+                dr.Render();
+                dr.Draw();
+            }
+        }
+        catch
+        {
+            error_counter++;
         }
     }
 
@@ -46,9 +55,70 @@ class DiagramWin : DrawingArea
         }
         return true;
     }
+}
 
-    void OnQuit (object sender, DeleteEventArgs e)
+class DiagramWin : Window
+{
+    string fileName;
+    DiagramArea da;
+    
+    MenuBar CreateMenuBar()
     {
-        Application.Quit ();
+        MenuBar mb = new MenuBar();
+        AccelGroup agrp = new AccelGroup();
+        AddAccelGroup(agrp);
+        
+        Menu fileMenu = new Menu();
+        MenuItem item = new MenuItem("_File");
+        item.Submenu = fileMenu;
+        mb.Append(item);
+
+        item = new ImageMenuItem(Stock.Quit, agrp);
+        item.Activated += OnMenuQuit;
+        fileMenu.Append(item);
+        
+        Menu viewMenu = new Menu();
+        item = new MenuItem("_View");
+        item.Submenu = viewMenu;
+        mb.Append(item);
+
+        item = new MenuItem("_Reload");
+        item.Activated += OnReload;
+        viewMenu.Append(item);
+        
+        return mb;
+    }
+    
+    public DiagramWin (string file_name) : base(file_name + " - smul")
+    {
+        this.fileName = file_name;
+        Diagram diagram = Program.Load(file_name);
+        
+        VBox v = new VBox();
+        MenuBar mb = CreateMenuBar();
+        da = new DiagramArea();
+        da.Diagram = diagram;
+        SetDefaultSize (600, 600);
+        DeleteEvent += new DeleteEventHandler (OnQuit);
+        v.PackStart(mb, false, false, 0);
+        v.PackStart(da, true, true, 0);
+        Add(v);
+        ShowAll();
+    }
+
+    void OnQuit(object sender, DeleteEventArgs e)
+    {
+        Application.Quit();
+    }
+
+    void OnMenuQuit(object o, EventArgs e)
+    {
+        Application.Quit();
+    }
+
+    void OnReload(object o, EventArgs e)
+    {
+        Diagram diagram = Program.Load(fileName);
+        da.Diagram = diagram;
     }
 }
